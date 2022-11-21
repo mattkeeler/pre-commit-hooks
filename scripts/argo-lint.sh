cb1bedcb978433fba48f2175591dccad2ebab22a
mattkeeler <19890779+mattkeeler@users.noreply.github.com>
2022-11-21 13:58:27 -0500

    Add argo lint hook (#5)

---
+++ scripts/argo-lint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# Runs `argo lint` against manifest files for Argo Workflows
+
+set -e -o pipefail
+
+if ! command -v argo &>/dev/null; then
+  echo "argo not found in your PATH" >&2
+  exit 1
+fi
+
+for f in "$@"; do
+    argo lint $f
+done
